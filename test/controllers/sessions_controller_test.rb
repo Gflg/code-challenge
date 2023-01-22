060d451102bcf615fce67a28cb3988ff73da09a4
Gflg <gflgon@gmail.com>
2023-01-22 15:09:48 -0300

    Create tests for Sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "should create session" do
+    post login_url, params: { user: { token: @user.confirmed_token } }
+    assert_redirected_to invoices_url
+  end
+
+  test "should get new" do
+    get login_url
+    assert_response :success
+  end
+
+  test "should destroy session" do
+    delete logout_url
+    assert_redirected_to root_url
+  end
 end
