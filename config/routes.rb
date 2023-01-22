69fac660fcc68aa1e6cf364653580a5a1f7a99c3
Gflg <gflgon@gmail.com>
2023-01-21 21:21:21 -0300

    Create user authentication by token

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   resources :users
   resources :invoices
+  
   get "/invoice_emails/:id" => "invoices#edit_emails", as: 'edit_emails'
   post "/invoice_emails/:id" => "invoices#save_emails", as: 'save_emails'
   get "/activate_token/:id" => "users#activate_token", as: 'activate_user_token'
+  
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
+
+  root :to => "sessions#new"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
